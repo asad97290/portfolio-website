3481e8d5a855b038a65001606b4253c77802d0c0
Asadullah Khan <aukhan@akru.co>
2023-12-28 11:01:40 +0500

    fix email margin

--- components/Home/SocialMediaArround/SocialMediaArround.tsx
+++ components/Home/SocialMediaArround/SocialMediaArround.tsx
@@ -60,7 +60,7 @@ export default function SocialMediaEmail(props: { finishedLoading: boolean }) {
               y: -3,
               transition: { y: { duration: 0.1 }, rotate: { duration: 0 } },
             }}
-            className=""
+            className="mb-10"
           >
             <a href="mailto:abdellatif@anaflous.com" target={"_blank"} rel="noreferrer">
               <span className=" font-Header tracking-wider text-gray-400 hover:text-AAsecondary hover:cursor-pointer">
