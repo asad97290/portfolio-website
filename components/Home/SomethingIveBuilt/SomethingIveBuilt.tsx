ee4d081d89331f3e047f98bdec009071e66cf1af
asadullah khan <asadullah.khan@tigercrypto.uk>
2024-05-24 22:42:42 +0500

    add some updates

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -30,8 +30,82 @@ export default function SomethingIveBuilt() {
       <div className="flex flex-col   xl:space-y-36 space-y-8 md:space-y-28">
 
 
-
         {/* // ?  Project 1 */}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              {/* <Link href={"/typing"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary 
+           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link> */}
+
+              <a href="https://pikamoon.io" target={"_blank"} rel="noreferrer">
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-30 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <Img src={"/pikamoon.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/pikamoon.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">PIKA MOON </span>
+                <a href="https://pikamoon.io" target="_blank" rel="noopener noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Blockchain Based AAA Game
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right ">
+                  At <span className="text-AAsecondary">Orbit Cosmos</span>, I played a crucial role in developing web3
+                  components for their Game application using <span className="text-AAsecondary"> Solidity, ethers.js, Wallet Connect and Wagmi.
+                  </span>  Our focus was on integrating new features and improving the user interface. Two notable
+                  achievements were, first the release of <span className="text-AAsecondary">Pikamoon Website V2</span>. Second Building  <span className="text-AAsecondary">Pikamoon Token and Staking  Platform</span>. This showcased
+                  our commitment to delivering innovative solutions. 
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">ERC20</span>
+                <span className="pr-4 z-10">NFT</span>
+                <span className="pr-4 z-10">Staking</span>
+                <span className="pr-4 z-10">Presale/ICO</span>
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+               
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* // ?  Project 2 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
           {/* Left image */}
           <div
@@ -108,7 +182,7 @@ export default function SomethingIveBuilt() {
           </div>
         </div>
 
-        {/* // ?  Project 2 */}
+        {/* // ?  Project 3 */}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96  ">
           {/* Left image */}
           <div
@@ -174,81 +248,7 @@ export default function SomethingIveBuilt() {
           </div>
         </div>
  
-         {/* // ?  Project 3 */}
-         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
-          {/* Left image */}
-          <div
-            className="hidden bg-AAprimary z-10  py-4 
-          absolute md:grid grid-cols-12 w-full h-full  content-center"
-          >
-            <div className="relative rounded w-full h-full col-span-7 ">
-              {/* <Link href={"/typing"}>
-                <div
-                  // onClick={}
-                  className="absolute w-full h-full rounded bg-AAsecondary 
-           transition-opacity opacity-20 hover:opacity-0 hover:cursor-pointer duration-300"
-                ></div>
-              </Link> */}
-
-              <a href="https://pikamoon.io" target={"_blank"} rel="noreferrer">
-                <div
-                  // onClick={}
-                  className="absolute w-full h-full rounded bg-AAprimary 
-           transition-opacity opacity-30 hover:opacity-0 hover:cursor-pointer duration-300"
-                ></div>
-              </a>
-
-              <Img src={"/pikamoon.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
-            </div>
-          </div>
-
-          {/* right Content */}
-          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
-            {/* background for text in mobile responsive */}
-            <div className="absolute w-full h-full bg-opacity-70 z-0">
-              <div className="relative w-full h-full">
-                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
-                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
-                <Img src={"/pikamoon.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
-              </div>
-            </div>
-
-            <div
-              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
-            col-span-8 flex flex-col items-start md:items-end space-y-3"
-            >
-              <div className="flex flex-col space-y-1 md:items-end z-10">
-                <span className="text-AAsecondary text-base">PIKA MOON </span>
-                <a href="https://pikamoon.io" target="_blank" rel="noopener noreferrer">
-                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
-                    Blockchain Based AAA Game
-                  </span>
-                </a>
-              </div>
-              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
-                <p className="text-gray-300 md:text-gray-400 text-left md:text-right ">
-                  At <span className="text-AAsecondary">Orbit Cosmos</span>, I played a crucial role in developing web3
-                  components for their Game application using <span className="text-AAsecondary"> Solidity, ethers.js, Wallet Connect and Wagmi.
-                  </span>  Our focus was on integrating new features and improving the user interface. Two notable
-                  achievements were, first the release of <span className="text-AAsecondary">Pikamoon Website V2</span>. Second Building  <span className="text-AAsecondary">Pikamoon Token and Staking  Platform</span>. This showcased
-                  our commitment to delivering innovative solutions. 
-                </p>
-              </div>
-              <ul
-                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
-               text-sm font-Text2 md:justify-end"
-              >
-                <span className="pr-4 z-10">ERC20</span>
-                <span className="pr-4 z-10">NFT</span>
-                <span className="pr-4 z-10">Staking</span>
-                <span className="pr-4 z-10">Presale/ICO</span>
-              </ul>
-              <div className="z-10 flex fle-row space-x-5 ">
-               
-              </div>
-            </div>
-          </div>
-        </div>
+       
  
        
       </div>
